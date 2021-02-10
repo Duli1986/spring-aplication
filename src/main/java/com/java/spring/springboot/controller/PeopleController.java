5223660bce4dbfb4a3c6580012d8f601082cf946
Duli1986 <dulovec.martin@gmail.com>
2021-02-10 19:38:03 +0100

    DB configuration

--- src/main/java/com/java/spring/springboot/controller/PeopleController.java
+++ src/main/java/com/java/spring/springboot/controller/PeopleController.java
@@ -4,10 +4,12 @@ import com.java.spring.springboot.model.People;
 import com.java.spring.springboot.repository.PeopleRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class PeopleController {
