43450f95860ba337f39826f992fc4ebc7858eb4f
Duli1986 <dulovec.martin@gmail.com>
2021-02-10 21:47:53 +0100

    add features

--- src/main/java/com/java/spring/springboot/controller/PeopleController.java
+++ src/main/java/com/java/spring/springboot/controller/PeopleController.java
@@ -6,6 +6,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,10 @@ public class PeopleController {
     @GetMapping("/peoples")
     public List<People> getAllPeople(){
         return peopleRepository.findAll();
+    }
+    @PostMapping("/peoples")
+    public People createPeople(@RequestBody People people){
+        return peopleRepository.save(people);
     }
 
 }
